58ccc6facf77860f40884647fcfe1952727e793b
Luckyyamgar <mhakuyamgar@gmail.com>
2023-06-16 09:59:41 +0530

    (second commit)

--- src/main/java/com/security/config/MyConfig.java
+++ src/main/java/com/security/config/MyConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,17 +17,29 @@ public class MyConfig {
 	@Bean
 	public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
 
+		//This code of using no restriction there
+		
+//		http.authorizeHttpRequests(authorize -> authorize
+//				
+//				.requestMatchers("/users/alluser/**").permitAll()
+//				
+//				//.authenticated()                                                 // .hasRole("EMPLOYEE")
+//                .anyRequest()
+//                .authenticated())
+//                //.permitAll())                                                   // .permitAll() PathRequest.toAntMatchers
+//				.formLogin(Customizer.withDefaults());
+//
+//		return http.build();
+		
+		//This code for Restriction of users/alluser Api
+		
 		http.authorizeHttpRequests(authorize -> authorize
-				
-				.requestMatchers("/users/alluser/**").permitAll()
-				
-				//.authenticated()                                                 // .hasRole("EMPLOYEE")
-                .anyRequest()
-                .authenticated())
-                //.permitAll())                                                   // .permitAll() PathRequest.toAntMatchers
-				.formLogin(Customizer.withDefaults());
+			    .requestMatchers("/users/alluser/**").hasRole("EMPLOYEE") // Restrict access to users with the "ADMIN" role
+			    .anyRequest().authenticated()
+			)
+			.formLogin(Customizer.withDefaults());
 
-		return http.build();	
+			return http.build();
 	}
 
 	@Bean
@@ -37,7 +50,9 @@ public class MyConfig {
 
 		UserDetails adminUser = User.withUsername("onkar").password(passwordEncoder().encode("onkar")).roles("MANAGER")
 				.build();
-		return new InMemoryUserDetailsManager(normaluser, adminUser);
+		UserDetails user= User.withUsername("pradip").password(passwordEncoder().encode("onkar")).roles("ADMIN").build();
+		
+		return new InMemoryUserDetailsManager(normaluser, adminUser,user);
 	}
 
 	@Bean
